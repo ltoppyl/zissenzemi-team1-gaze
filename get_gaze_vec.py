8fc8bf9ebf94b70f545db40c527d88aed42f5d58
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-09-08 17:59:05 +0900

    ファイル間の繋がりの作成 (#4)
    * update READMEへの内容の追加
    
    * update 全体のファイル間の繋がりの形成
    
    * update judging_vecとget_gaze_vec間の繋がりの作成

--- get_gaze_vec.py
+++ get_gaze_vec.py
@@ -1,2 +1,8 @@
-from ailia_models.face_recognition.ax_gaze_estimation import ax_gaze_estimation
+def get_gaze_vec():
+    from ailia_models.face_recognition.ax_gaze_estimation import ax_gaze_estimation_functionalize
+    import judging_out
 
+    gaze_vec = ax_gaze_estimation_functionalize.get_vec()
+    print(gaze_vec)
+
+    judging_out.judging_out(gaze_vec)
