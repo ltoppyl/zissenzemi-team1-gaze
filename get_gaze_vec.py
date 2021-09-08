43ea0582c6c8eab85383c4fee08486d7008182f2
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-09-08 23:42:07 +0900

    fix コンフリクトの解消

--- get_gaze_vec.py
+++ get_gaze_vec.py
@@ -1,8 +1,8 @@
 def get_gaze_vec():
-    from ailia_models.face_recognition.ax_gaze_estimation import ax_gaze_estimation_functionalize
+    from ax_gaze_estimation import ax_gaze_estimation_functionalize
     import judging_out
 
-    gaze_vec = ax_gaze_estimation_functionalize.get_vec()
-    print(gaze_vec)
+    gaze_vec = ax_gaze_estimation_functionalize.main()
 
-    judging_out.judging_out(gaze_vec)
+
+    # judging_out.judging_out(gaze_vec)
