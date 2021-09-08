24d9ebbe44826c1884ddf4deff315865c03e46f7
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-09-09 00:07:14 +0900

    fix カメラ起動後落ちるバグの修正

--- get_gaze_vec.py
+++ get_gaze_vec.py
@@ -1,5 +1,5 @@
 def get_gaze_vec():
-    from ax_gaze_estimation import ax_gaze_estimation_functionalize
+    import ax_gaze_estimation_functionalize
     import judging_out
 
     gaze_vec = ax_gaze_estimation_functionalize.main()
